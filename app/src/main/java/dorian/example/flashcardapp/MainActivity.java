d5aac21e6b8c39b8593a389a9fb2bf3f332e3706
Smalley2001 <51380202+Smalley2001@users.noreply.github.com>
2020-02-21 00:22:51 -0500

    First commit

---
+++ app/src/main/java/dorian/example/flashcardapp/MainActivity.java
@@ -0,0 +1,24 @@
+package dorian.example.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                // Do something here
+            }
+        });
+    }
+}
